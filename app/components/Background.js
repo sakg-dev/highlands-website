d01ec539eed0356420f7239571cbbe480cbfaece
sakg-dev <sakg.dev.14@gmail.com>
2026-09-08 01:47:04 +0530

    able to modify y

--- app/components/Background.js
+++ app/components/Background.js
@@ -1,8 +1,16 @@
 import Image from 'next/image';
+import { useEffect, useRef } from 'react';
 
-export const Background = () => {
+export const Background = ({ dy }) => {
+    const imageRef = useRef()
+    useEffect(()=>{
+        if(imageRef.current){
+            const currentVal = parseInt(imageRef.current.style.top.split("px")[0])
+            imageRef.current.style.top = `${currentVal+dy}px`
+        }
+    },[dy])
     return (
-        <div className="background fixed -top-1100 left-0 -z-1 prevent-select">
+        <div ref={imageRef} style={{ top: -4250 }} className="background fixed left-0 -z-1 prevent-select">
             <Image className="w-full" src="/mountain.jpeg" height={5000} priority={true} width={1900} alt="Mountain_Img"/>
         </div>
     )
--- app/page.jsx
+++ app/page.jsx
@@ -2,15 +2,33 @@
 import { Hero } from '@/app/components/Hero.js'
 import { HowItWorks } from '@/app/components/HowItWorks.js'
 import { Background } from '@/app/components/Background.js'
+import { useEffect, useState, useRef } from 'react'
 
 export default function Home() {
     let sections = [
         Hero,
         HowItWorks
     ]
+
+    const scrollY = useRef(0);
+    const [dy, setDy] = useState(0);
+
+    useEffect(()=>{
+        const handleScroll = (event)=>{
+            // console.log("scrolling")
+            // console.log(event)
+            const oldY = scrollY.current;
+            scrollY.current = window.scrollY;
+            setDy(scrollY.current - oldY);
+        }
+        window.addEventListener("scroll", handleScroll)
+        // TODO: rmv the event when return
+    }, [])
+
+
     return (
         <div className="overflow-x-hidden">
-            <Background />
+            <Background dy={dy} />
             <div className="flex flex-col">
                 {sections.map((_, idx)=>{
                     let Comp = sections[idx]
