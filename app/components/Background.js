940176bb4cd8e8a2605c47cba736f6a0a2fb8233
sakg-dev <sakg.dev.14@gmail.com>
2026-09-23 10:40:34 +0530

    move different layers differently according to their sections, height and other

--- app/components/Background.js
+++ app/components/Background.js
@@ -6,16 +6,32 @@ import { getImgSize } from '@/lib.js';
 // const { height, width } = await getImgSize("./public/mountain.jpeg")
 
 
-const Layer = ({ layer }) => {
+const Layer = ({ layer, progress }) => {
     const { name, zIndex, imgLink, start, end, ref, height, width } = layer;
     useEffect(()=>{
-        setInterval(()=>{
-            const top = parseInt(ref.current.style.top.split("px")[0])
-            ref.current.style.top = `${top+1}px`
-        }, 5)
-    }, [])
-    return <div className="fixed left-0" style={{ top: 0, zIndex: zIndex }} ref={ref}>
-        <Image className="w-full" src={imgLink} height={height} priority={true} width={width} alt="Mountain_Img"/>
+        const viewportHeight = window.innerHeight;
+        const siteHeight = document.body.scrollHeight;
+
+        if(ref.current) {
+            console.log(`need to move ${name}`)
+
+            let sectionProgress = (progress - start) / (end - start);
+            console.log(`${sectionProgress}: ${name}`)
+
+            if(height - viewportHeight > 0) {
+                // ref.current.style.top = `${(viewportHeight*1.5) - height}px`
+                ref.current.style.top = `${((viewportHeight*1.5)-height) * (1-sectionProgress)}px`
+                // 0 -> -height + viewportHeight
+                // 0.5 -> -height * 0.5
+                // console.log(ref.current.style.top);
+            } else {
+                ref.current.style.top = `${( (viewportHeight*1.05) - height ) * (1+sectionProgress)}px`
+                // 0 -> 
+            }
+        }
+    }, [progress])
+    return <div className="fixed w-full" style={{ zIndex: zIndex }} ref={ref}>
+        <Image src={imgLink} height={height} priority={true} width={width} alt={name} />
     </div>
 }
 
@@ -28,7 +44,7 @@ export const Background = ({ progress }) => {
             zIndex: 3,
             imgLink: "/grassland.png",
             start: 0,
-            end: 0.1,
+            end: 0.2,
             ref: useRef(),
             height: 480,
             width: 1920
@@ -38,24 +54,17 @@ export const Background = ({ progress }) => {
             zIndex: 2,
             imgLink: "/mountain.jpeg",
             start: 0,
-            end: 0.9,
+            end: 0.8,
             ref: useRef(),
             height: 5400,
             width: 1920
         }
     ];
 
-    /*useEffect(()=>{
-        if(imageRef.current){
-            const screenHeight = screen.height * 1.5;
-            imageRef.current.style.top = `-${(imgSize.height - screenHeight) * (1-progress)}px` 
-        }
-    },[progress]);
-    */
     return (
         <div className="background w-screen fixed left-0 top-0 -z-1 prevent-select">
             {layers.map((layer, idx)=>{
-                return <Layer layer={layer} key={idx} />    
+                return <Layer progress={progress} layer={layer} key={idx} />    
             })}
         </div>
     );
